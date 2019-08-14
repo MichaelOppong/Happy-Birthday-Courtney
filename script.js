5069761a217431ed05ed00e77294cef66771ba39
MichaelOppong <michael@scripted.org>
2019-08-14 13:56:41 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,4 @@
+$("button").click(function() {
+    $(".pictures").append(`<div><h1>2016 Bonus</h1><img src="https://i.ibb.co/V3y3bTv/IMG-0316.jpg"></div><div><h1>2020 Placeholder</h1><img src="https://media.giphy.com/media/OgfejiBYdTHXO/giphy.gif"></div>`);
+
+});
